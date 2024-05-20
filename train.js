f631e06eb9dfcf5892340cc39bf7332ad85ed19c
jurabek10 <samiev.jurabek@bk.ru>
2024-05-20 18:36:10 +0900

    Done Task-A

--- train.js
+++ train.js
@@ -1,44 +1,78 @@
-console.log("Jack Ma maslahtlari");
+/*
+A-TASK: 
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi 
+parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
 
-const list = [
-  "Yaxshi talaba bo'ling", // 0-20
-  "To'g'ri boshliq tanlang", // 20-30
-  "O'zingiz ishlashni boshlang", // 30-40
-  "Siz kuchli bo'gan narsalarni qiling", // 40-50
-  "Yoshlarga inverstitsiya qiling", // 50-60
-  "dam oling", // 60
-];
+*/
 
-// CLLBACK FUNCTIONS
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("Insert number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a < 20 && a <= 30) callback(null, list[1]);
-  else if (a < 30 && a <= 40) callback(null, list[2]);
-  else if (a < 40 && a <= 50) callback(null, list[3]);
-  else if (a < 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 5000);
+// JAVOB
+
+function countLetter(a, b) {
+  let count = 0;
+  const lowerLetter = a.toLowerCase();
+
+  for (let i = 0; i < b.length; i++) {
+    if (b[i].toLowerCase() === lowerLetter) {
+      count++;
+    }
+    // else if (!b.includes(lowerLetter)) {
+    //   return `${b} does not include ${a}`;
+    // }
+    else if (count === 0) {
+      return `${b} does not include ${a}`;
+    }
   }
+
+  return `There are ${count} ${a} letters in the word of ${b}`;
 }
 
-console.log("passed here 0");
-maslahatBering(20, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log(data);
-  }
-});
+console.log(countLetter("a", "ali"));
+console.log(countLetter("a", "eli"));
+console.log(countLetter("e", "engineer"));
+console.log(countLetter("I", "I study in MIT"));
 
-console.log("passed here 0");
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log(data);
-  }
-});
+// console.log("Jack Ma maslahtlari");
+
+// const list = [
+//   "Yaxshi talaba bo'ling", // 0-20
+//   "To'g'ri boshliq tanlang", // 20-30
+//   "O'zingiz ishlashni boshlang", // 30-40
+//   "Siz kuchli bo'gan narsalarni qiling", // 40-50
+//   "Yoshlarga inverstitsiya qiling", // 50-60
+//   "dam oling", // 60
+// ];
+
+// // CLLBACK FUNCTIONS
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("Insert number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a < 20 && a <= 30) callback(null, list[1]);
+//   else if (a < 30 && a <= 40) callback(null, list[2]);
+//   else if (a < 40 && a <= 50) callback(null, list[3]);
+//   else if (a < 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(20, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log(data);
+//   }
+// });
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log("ERROR:", err);
+//   else {
+//     console.log(data);
+//   }
+// });
 
 // ASYNC FUNCTIONS (DEFINE)
 // async function maslahatBering(a) {
