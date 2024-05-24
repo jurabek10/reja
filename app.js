56a8ccea7b6929815f064d501c7d809940458074
jurabek10 <samiev.jurabek@bk.ru>
2024-05-24 23:18:08 +0900

    feat: add frontEnd javascript

--- app.js
+++ app.js
@@ -32,12 +32,8 @@ app.post("/create-item", (req, res) => {
   // console.log(req.body);
   const newReja = req.body.reja;
   db.collection("plans").insertOne({ reja: newReja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("Something went wrong");
-    } else {
-      res.end("successfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
 });
 
